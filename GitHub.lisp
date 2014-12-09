c3298eb65850c4ece149157d3ac1dd5e579dfed1
James Treasure <jandrewtreasure@gmail.com>
2014-12-09 13:02:18 +0000

    Fixed atom-less to take 0 value polynomials

--- GitHub.lisp
+++ GitHub.lisp
@@ -10,14 +10,20 @@
                   (t1(car(cdr m1)))
                   (t2(car(cdr m2))))
 
-                (if (equal (sort t1 #'llt) (sort t2 #'llt))                                            ; if t1 == t2
+                (if (equal t1 t2)
                     (collect (append (list (list (+ c1 c2) t1)) rem))          ; then return collect((c1 + c2, t1) + rem)
                     (append (list m1) (collect (append (list m2) rem))))))))   ; else return (c1, t1) + collect((c2, t2) + rem)
 
 (defun atom-less (a b)
- (if (realp a)
-  (< a b)
-  (string-lessp a b)))
+  (cond
+    ((null a) t)
+    ((null b) t)
+    ((if (realp a)
+      (< a b)
+      (string-lessp a b)
+    ))
+  )
+)
 
 (defun llt (a b)
  (cond
@@ -31,11 +37,11 @@
 (defun compare(a b)
   (llt (car (cdr a)) (car (cdr b))))
 
-(defun sortpoly(a)
+(defun sortcollect(a)
   (sort a #'compare))
 
 (defun p+(a b)
-  (my-remove(collect(sortpoly(append a b)))))
+  (my-remove(collect(sortcollect(append a b)))))
 
 (defun my-remove(a)
   (if (null a)
@@ -50,7 +56,17 @@
 (defun p-(a b)
   (p+ a (negate b)))
 
-(print(p+'((10((5 y)(3 x)(2 z)))) '((10((2 z)(3 x)(5 y))))))
 
+(defun term-multiply(a b)
+  (list (* (car a) (car b)) (collect(sortcollect(append (car(cdr a)) (car(cdr b)))))))
+
+
+;(print(p-'((10((5 y)(3 x)(2 z)))) '((10((5 y)(3 x)(2 z))))))
+;(print(car(car(cdr '((5((2 x)))(10((3 y))))))))
+
+;(print(term-multiply '(5((1 x))) '(2(()))))
+
+(print(p+ '((5((1 x)))(2())) '((2((1 y)))(3((1 x)))(7()))))
 
 ;p- (p+ p1 p2) p2)
+(print(p+ '((1())) '((2()))))
