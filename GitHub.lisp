308ae39b01f54b13fadbde2d62b839db32ee1e3a
James Treasure <jandrewtreasure@gmail.com>
2014-12-10 18:13:09 +0000

    Comment and reorganise code

--- GitHub.lisp
+++ GitHub.lisp
@@ -1,30 +1,39 @@
+;Function to collect like terms
 (defun collect (l)
+    ;extract first list, second list and the remainder
     (let ((m1(car l))
           (m2(car(cdr l)))
           (rem(cdr(cdr l))))
     
-        (if (null m2)                                                          ; If the list has less than 2 elements
-            l                                                                  ; then return the list (nothing to collect)
-            (let ((c1(car m1))                                                 ; else, extract (c1, t1), (c2, t2) from m1 and m2
+        ; If the list has less than 2 elements
+        (if (null m2)
+            ; then return the list (nothing to collect)                                                          
+            l
+            ; else, extract (c1, t1), (c2, t2) from m1 and m2                                                                 
+            (let ((c1(car m1))                                                 
                   (c2(car m2))
                   (t1(car(cdr m1)))
                   (t2(car(cdr m2))))
 
                 (if (equal t1 t2)
-                    (collect (append (list (list (+ c1 c2) t1)) rem))          ; then return collect((c1 + c2, t1) + rem)
-                    (append (list m1) (collect (append (list m2) rem))))))))   ; else return (c1, t1) + collect((c2, t2) + rem)
+                    ; then return collect((c1 + c2, t1) + rem)
+                    (collect (append (list (list (+ c1 c2) t1)) rem))
+                    ; else return (c1, t1) + collect((c2, t2) + rem)          
+                    (append (list m1) (collect (append (list m2) rem))))))))
 
+;Function to compare variables against each other, such that a < b
 (defun atom-less (a b)
   (cond
     ((null a) t)
     ((null b) t)
+    ;if a is a number
     ((if (realp a)
+      ;return a < b
       (< a b)
-      (string-lessp a b)
-    ))
-  )
-)
+      ;else use string-lessp which compares lexicograhpically such that a < b, c < d, x < y etc
+      (string-lessp a b)))))
 
+;list less than function, returns true if first list is less than the second
 (defun llt (a b)
  (cond
   ((null a) nil)
@@ -34,40 +43,52 @@
    ((equal (car a) (car b)) (llt (cdr a) (cdr b)))
    (nil)))))
 
+;compares two terms
 (defun compare(a b)
   (llt (car (cdr a)) (car (cdr b))))
 
+;sorts the terms so that the collect function can work.
 (defun sortcollect(a)
   (sort a #'compare))
 
-(defun p+(a b)
-  (my-remove(collect(sortcollect(append a b)))))
-
+;Removes a term in a polynomial if the coefficient is 0
 (defun my-remove(a)
   (if (null a)
     a
+    ;if the coefficient is equal to 0
     (if (= (car(car a)) 0)
+      ;recurse on my remove with the rest of the list
       (my-remove (cdr a))
+      ;else add to the list and recurse on the remainder
       (append (list (car a)) (my-remove (cdr a))))))
 
+;Function to negate a polynomial. This allows subtraction to implemented via additon.
 (defun negate(a)
+  ;Makes every coefficient in a polynomial negative
   (map 'list (lambda (x) (list (- (car x)) (car (cdr x)))) a))
 
-(defun p-(a b)
-  (p+ a (negate b)))
-
+;multiplies two terms together
 (defun term-multiply(a b)
   (list (* (car a) (car b)) (collect(sortcollect(append (car(cdr a)) (car(cdr b)))))))
 
+;multiplies a term by a polynomial
 (defun term-polynomial (poly term) 
   (map 'list #'(lambda (x) (term-multiply x term)) poly))
 
+
+;addition function
+(defun p+(a b)
+  (my-remove(collect(sortcollect(append a b)))))
+
+;subtraction function
+(defun p-(a b)
+  (p+ a (negate b)))
+
+;multiplication function
 (defun p* (a b)
   (if (null b) 
     nil
     (p+ (term-polynomial a (car b)) (p* a (cdr b)))))
 
-(print(p* '((5((2 x)))(3((3 y)))) '((5((2 x)))(2((3 x))))))
-;(print(term-polynomial '((5((2 x)))(3((2 y)))) '(5((2 x)))))
+(print(p- '((1.5((0 x)(0 y)))) '((3.2((2 X))))))
 
-;so take the first term of the first polynomial, multiply it with the other polynomial, recurse on the remainder of the first polynomial and add these two result polynomials together
